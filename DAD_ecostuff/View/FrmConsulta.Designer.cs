17935ffcd407dc53ce0ca6c248f7eecdf47ed0b3
GuilhermXd <guilherlie@gmail.com>
2024-10-05 21:55:15 -0300

    exclusão de Plantas Adicionado

--- DAD_ecostuff/View/FrmConsulta.Designer.cs
+++ DAD_ecostuff/View/FrmConsulta.Designer.cs
@@ -31,29 +31,33 @@ namespace View
         {
             this.components = new System.ComponentModel.Container();
             this.dgPlanta = new System.Windows.Forms.DataGridView();
+            this.plantaBindingSource5 = new System.Windows.Forms.BindingSource(this.components);
             this.plantaBindingSource4 = new System.Windows.Forms.BindingSource(this.components);
             this.plantaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.plantaBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.plantaRepositoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.plantaBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
             this.plantaBindingSource3 = new System.Windows.Forms.BindingSource(this.components);
-            this.plantaBindingSource5 = new System.Windows.Forms.BindingSource(this.components);
+            this.plantaBindingSource6 = new System.Windows.Forms.BindingSource(this.components);
             this.codigoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Nomecientifico = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.observacoesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ciclodeaguaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cicloiluminacaoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.tiposoloDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.datadeverificacaoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomecientificoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgPlanta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaRepositoryBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource6)).BeginInit();
             this.SuspendLayout();
             // 
             // dgPlanta
@@ -64,18 +68,23 @@ namespace View
             this.dgPlanta.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.codigoDataGridViewTextBoxColumn,
             this.nomeDataGridViewTextBoxColumn,
-            this.Nomecientifico,
-            this.observacoesDataGridViewTextBoxColumn,
-            this.ciclodeaguaDataGridViewTextBoxColumn,
-            this.cicloiluminacaoDataGridViewTextBoxColumn,
-            this.tiposoloDataGridViewTextBoxColumn,
-            this.datadeverificacaoDataGridViewTextBoxColumn});
-            this.dgPlanta.DataSource = this.plantaBindingSource5;
-            this.dgPlanta.Location = new System.Drawing.Point(77, 254);
+            this.nomecientificoDataGridViewTextBoxColumn,
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.dgPlanta.DataSource = this.plantaBindingSource6;
+            this.dgPlanta.Location = new System.Drawing.Point(12, 40);
             this.dgPlanta.Name = "dgPlanta";
             this.dgPlanta.ReadOnly = true;
-            this.dgPlanta.Size = new System.Drawing.Size(864, 150);
+            this.dgPlanta.Size = new System.Drawing.Size(845, 150);
             this.dgPlanta.TabIndex = 0;
+            this.dgPlanta.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgPlanta_CellClick);
+            // 
+            // plantaBindingSource5
+            // 
+            this.plantaBindingSource5.DataSource = typeof(Dados.Planta);
             // 
             // plantaBindingSource4
             // 
@@ -101,17 +110,16 @@ namespace View
             // 
             this.plantaBindingSource3.DataSource = typeof(Dados.Planta);
             // 
-            // plantaBindingSource5
+            // plantaBindingSource6
             // 
-            this.plantaBindingSource5.DataSource = typeof(Dados.Planta);
+            this.plantaBindingSource6.DataSource = typeof(Dados.Planta);
             // 
             // codigoDataGridViewTextBoxColumn
             // 
             this.codigoDataGridViewTextBoxColumn.DataPropertyName = "Codigo";
-            this.codigoDataGridViewTextBoxColumn.HeaderText = "Código";
+            this.codigoDataGridViewTextBoxColumn.HeaderText = "Codigo";
             this.codigoDataGridViewTextBoxColumn.Name = "codigoDataGridViewTextBoxColumn";
             this.codigoDataGridViewTextBoxColumn.ReadOnly = true;
-            this.codigoDataGridViewTextBoxColumn.Width = 50;
             // 
             // nomeDataGridViewTextBoxColumn
             // 
@@ -120,50 +128,66 @@ namespace View
             this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
             this.nomeDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // Nomecientifico
+            // nomecientificoDataGridViewTextBoxColumn
             // 
-            this.Nomecientifico.DataPropertyName = "NOME_CIENTIFICO";
-            this.Nomecientifico.HeaderText = "Nome Científico";
-            this.Nomecientifico.Name = "Nomecientifico";
-            this.Nomecientifico.ReadOnly = true;
-            this.Nomecientifico.Width = 120;
+            this.nomecientificoDataGridViewTextBoxColumn.DataPropertyName = "Nome_cientifico";
+            this.nomecientificoDataGridViewTextBoxColumn.HeaderText = "Nomecientifico";
+            this.nomecientificoDataGridViewTextBoxColumn.Name = "nomecientificoDataGridViewTextBoxColumn";
+            this.nomecientificoDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // observacoesDataGridViewTextBoxColumn
+            // dataGridViewTextBoxColumn1
             // 
-            this.observacoesDataGridViewTextBoxColumn.DataPropertyName = "OBSERVACOES";
-            this.observacoesDataGridViewTextBoxColumn.HeaderText = "Observações";
-            this.observacoesDataGridViewTextBoxColumn.Name = "observacoesDataGridViewTextBoxColumn";
-            this.observacoesDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Observacoes";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Observacoes";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
             // 
-            // ciclodeaguaDataGridViewTextBoxColumn
+            // dataGridViewTextBoxColumn2
             // 
-            this.ciclodeaguaDataGridViewTextBoxColumn.DataPropertyName = "ciclo_DE_AGUA";
-            this.ciclodeaguaDataGridViewTextBoxColumn.HeaderText = "Ciclo de água";
-            this.ciclodeaguaDataGridViewTextBoxColumn.Name = "ciclodeaguaDataGridViewTextBoxColumn";
-            this.ciclodeaguaDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Data_da_verificacao";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Datadeverificacao";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
             // 
-            // cicloiluminacaoDataGridViewTextBoxColumn
+            // dataGridViewTextBoxColumn3
             // 
-            this.cicloiluminacaoDataGridViewTextBoxColumn.DataPropertyName = "CICLO_DE_ILUMINACAO";
-            this.cicloiluminacaoDataGridViewTextBoxColumn.HeaderText = "Ciclo de Iluminacao";
-            this.cicloiluminacaoDataGridViewTextBoxColumn.Name = "cicloiluminacaoDataGridViewTextBoxColumn";
-            this.cicloiluminacaoDataGridViewTextBoxColumn.ReadOnly = true;
-            this.cicloiluminacaoDataGridViewTextBoxColumn.Width = 130;
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Ciclo_de_agua";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Ciclodeagua";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
             // 
-            // tiposoloDataGridViewTextBoxColumn
+            // dataGridViewTextBoxColumn4
             // 
-            this.tiposoloDataGridViewTextBoxColumn.DataPropertyName = "TIPO_DE_SOLO";
-            this.tiposoloDataGridViewTextBoxColumn.HeaderText = "Tipo de Solo";
-            this.tiposoloDataGridViewTextBoxColumn.Name = "tiposoloDataGridViewTextBoxColumn";
-            this.tiposoloDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Ciclo_de_iluminacao";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Cicloiluminacao";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
             // 
-            // datadeverificacaoDataGridViewTextBoxColumn
+            // dataGridViewTextBoxColumn5
             // 
-            this.datadeverificacaoDataGridViewTextBoxColumn.DataPropertyName = "DATA_DA_VERIFICACAO";
-            this.datadeverificacaoDataGridViewTextBoxColumn.HeaderText = "Data de Consulta";
-            this.datadeverificacaoDataGridViewTextBoxColumn.Name = "datadeverificacaoDataGridViewTextBoxColumn";
-            this.datadeverificacaoDataGridViewTextBoxColumn.ReadOnly = true;
-            this.datadeverificacaoDataGridViewTextBoxColumn.Width = 120;
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Tipo_de_solo";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Tiposolo";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(12, 219);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(108, 47);
+            this.btnExcluir.TabIndex = 1;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // FrmConsulta
             // 
@@ -171,19 +195,22 @@ namespace View
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1043, 450);
             this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnExcluir);
             this.Controls.Add(this.dgPlanta);
             this.Name = "FrmConsulta";
             this.ShowIcon = false;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FrmConsulta_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgPlanta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaRepositoryBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plantaBindingSource6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -197,13 +224,21 @@ namespace View
         private System.Windows.Forms.DataGridView dgPlanta;
         private System.Windows.Forms.BindingSource plantaBindingSource4;
         private System.Windows.Forms.BindingSource plantaBindingSource5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn codigoDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Nomecientifico;
         private System.Windows.Forms.DataGridViewTextBoxColumn observacoesDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn ciclodeaguaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn cicloiluminacaoDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn tiposoloDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn datadeverificacaoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource plantaBindingSource6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomecientificoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button button2;
     }
 }
--- DAD_ecostuff/View/FrmConsulta.cs
+++ DAD_ecostuff/View/FrmConsulta.cs
@@ -30,10 +30,41 @@ namespace View
             //dgPlanta.DataSource = _plantaService.getAll();
             dgPlanta.Refresh();
         }
+        private void atualiza() {
+            dgPlanta.DataSource = _plantaService.getAll();
+        }
 
         private void FrmConsulta_Load(object sender, EventArgs e)
         {
             carregaGridView();
         }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            DialogResult msg = MessageBox.Show("Deseja confirmar exclusão?", "Informação", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (msg == DialogResult.Yes)
+            {
+                _plantaService.Remove(codigo);
+                atualiza();
+
+            }
+            else {
+                msg = MessageBox.Show("Exclusão cancelada!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        int codigo;
+        private void dgPlanta_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            if (e.RowIndex <= 0) 
+            {
+                return;
+            }
+            string id = dgPlanta.Rows[e.RowIndex].Cells[0].Value.ToString();
+            codigo = int.Parse(id);
+            dgPlanta.Refresh();
+        }
+
     }
 }
